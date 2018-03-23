374b94674de961d8ef4500ff3cbde4b2ea09174b
Valentin Vasilev <valentin.v1997@gmail.com>
2018-03-23 18:57:05 +0200

    Changed the LinearRegression.app namespace to LinearRegression.App and added Material UI Elements package

--- LinearRegression.app/MainWindow.xaml.cs
+++ LinearRegression.app/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LinearRegression.app
+namespace LinearRegression.App
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
