4231d03bfcb43f14f609b09c70d7c5eb31994c78
vv-b-s <valentin.v1997@gmail.com>
2018-05-07 17:47:54 +0300

    Added example for AnalysisLogic implementation

--- LinearRegression.App/ServiceAdapters/AnalysisLogicService.cs
+++ LinearRegression.App/ServiceAdapters/AnalysisLogicService.cs
@@ -1,7 +1,9 @@
 ﻿using LinearRegression.App.Contracts;
 using LinearRegression.App.Contracts.Services;
+using LinearRegression.App.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,18 @@ namespace LinearRegression.App.ServiceAdapters
 
         public IAnalysisData<IAdjustedDataRow> GetAdjustedData(IAnalysisData<IAnalysisDataRow> rawAnalysisModel)
         {
+            //var xdata = rawAnalysisModel.Data.Select(d => d.X).ToArray();
+            //var ydata = rawAnalysisModel.Data.Select(d => d.Y).ToArray();
+
+            //List<double> ys = NikiService.GetAdjustedY(xdata, ydata);
+
+            //var collection = new ObservableCollection<IAdjustedDataRow>();
+
+            //for (int i = 0; i < ys.Count; i++)
+            //    collection.Add(new AdjustedDataRow(i + 1, xdata[i], ydata[i], ys[i]));
+
+            //return new FullAnalysis<IAdjustedDataRow>(12, null, null, null, null, collection);
+
             //TODO: use calculations to get the adjusted data table or check if it already exists in the database
             throw new NotImplementedException();
         }
